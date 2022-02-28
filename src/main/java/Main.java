e635e41edd61d49976515f392e42cbe6c7190597
Antonio Procentese <anto.proce@gmail.com>
2022-02-28 15:52:51 +0100

    added Main class

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String... args) {
+        System.out.println(HelloWorld.hello("Riccardo"));
+        System.out.println(HelloWorld.hello("Antonio"));
+        System.out.println(HelloWorld.hello());
+    }
+}
